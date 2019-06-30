848591f285f159cd69ef011fb091d6e4ea3ebe53
tnfaid <nurfaidahtitik@gmail.com>
2019-06-30 20:03:39 +0700

    splash screen

---
+++ app/src/main/java/titik/com/pantaupadi/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package titik.com.pantaupadi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_opening);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        timer.start();
+    }
+}
