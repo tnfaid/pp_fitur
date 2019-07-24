0ff99a57b97dfb5d60c069c0c4b7626018fd6822
tnfaid <nurfaidahtitik@gmail.com>
2019-07-25 02:18:38 +0700

    Manifest and ActivityDetectionContour

---
+++ app/src/main/java/titik/com/pantaupadi/Activity/Detection/ActivityDetectionContour.java
@@ -0,0 +1,27 @@
+package titik.com.pantaupadi.Activity.Detection;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import titik.com.pantaupadi.Activity.DetectionResultHolder;
+import titik.com.pantaupadi.Adapter.DetectionDetailsListAdapter;
+import titik.com.pantaupadi.R;
+
+public class ActivityDetectionContour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detection_details);
+        if (DetectionResultHolder.detectionResultAvailable()) {
+            ListView detectionDetailsList = (ListView) findViewById(R.id.detection_deteils_activity_list);
+
+            DetectionDetailsListAdapter adapter = new DetectionDetailsListAdapter(this, DetectionResultHolder.getDetectionResult().getDetectionStepDetails());
+
+            detectionDetailsList.setAdapter(adapter);
+        }
+    }
+}
